61d1d6637bf10755eaab5157d75e32cc1db489b7
Ted Zhu <tedz2usa@gmail.com>
2016-02-01 16:49:06 -0500

    Clean up code.

--- dombox.js
+++ dombox.js
@@ -1,23 +1,15 @@
 var log = console.log.bind(console);
 
-log('Hello!');
-
 window.addEventListener('load', init);
 
-var body;
-
 function init() {
-	log('Window loaded.');
-	body  = document.getElementsByTagName('body')[0];
-	var box = createBox('test', body);
+	var box = createBox('test', document.body);
 }
-
 
 function createBox(classname, parent) {
 	var elem = document.createElement('div');
 	elem.classList.add(classname);
-	parent.appendChild(elem)
-
+	parent.appendChild(elem);
 }
 
 
